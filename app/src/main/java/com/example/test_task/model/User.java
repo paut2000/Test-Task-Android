653f3ab343261967254b40a3dd3199be63c6a304
molner <molner.vasilii@gmail.com>
2022-08-10 10:45:02 +0700

    model created

---
+++ app/src/main/java/com/example/test_task/model/User.java
@@ -0,0 +1,15 @@
+package com.example.test_task.model;
+
+import java.util.ArrayList;
+
+public class User {
+
+    private String id;
+
+    private String username;
+
+    private String email;
+
+    private ArrayList<String> permissions;
+
+}
